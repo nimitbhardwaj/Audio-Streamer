d7b1a91fbf034929fa54e3b3fe9aff53234a34ca
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2017-03-21 22:56:07 +0530

    commit

--- microphone.py
+++ microphone.py
@@ -4,8 +4,6 @@ import threading
 class Microphone(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.sample = None
-        self.inputSample = None
         CHUNK = 1024
         RATE = 44100
         FORMAT = pyaudio.paInt16
@@ -17,11 +15,15 @@ class Microphone(threading.Thread):
                         input = True,
                         output = True,
                         frames_per_buffer = CHUNK)
+        self.sample = self.stream.read(CHUNK)
+        self.inputSample = self.stream.read(CHUNK)
+        self.kapa = self.inputSample
     def run(self):
         CHUNK = 1024
+        kapa = self.inputSample
         while True:
             self.sample = self.stream.read(CHUNK)
-            if self.inputSample == None:
+            if self.inputSample == self.kapa:
                 continue
             self.stream.write(self.inputSample, CHUNK)
         self.stream.stop_stream()
